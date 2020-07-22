a3da39b4b706a02fd37da065e6c0e001c587ced8
Ollin <ollin.langle@ciencias.unam.mx>
2020-07-21 21:44:25 -0400

    update Agent

--- src/NetABM.jl
+++ src/NetABM.jl
@@ -2,10 +2,11 @@ __precompile__()
 module NetABM
     using LightGraphs, Random, DelimitedFiles, StatsBase, SparseArrays, Distributions
     export lectura_uw, lfr_network, Agent, Params
-    export init_demographics!
+    export init_demographics!, get_coop!, next_state!
     export initialize_demographics!, set_fixed_coop_agents!, set_coop_agents!
     export assign_contacts!, get_next_state!, update_state!
     export update_all_agents!, get_populations, export_parameters
+
 
     """
         Agent(id, neighs, p_cop, state)
@@ -26,17 +27,51 @@ module NetABM
         id          ::Int64
         state       ::String
         new_state   ::String
+        previous    ::Array{String}
         num_meets   ::Int64
         recovery_t  ::Int64
         infection_t ::Int64
-        contacts_t  ::Vector{Agent}
+        contacts_t  ::Vector{Int64}
+        coopf       ::Vector{Int64}
+        non_coopf   ::Vector{Int64}
         age_group   ::Int64
         degree_t    ::Int64
         p_cop       ::Float64
         at_home     ::Bool
         # DEFAULT CONSTRUCTOR
-        Agent(id) = new(id, "S", "S", 1, 5, 0, Vector{Agent}(), 1, 0, 1.0, false)
+        Agent(id) = new(id, "S", "S", Vector{String}(), 1, 5, 0, Vector{Int64}(), Vector{String}(), Vector{String}(), 1, 0, 1.0, false)
     end
+
+    #  """
+    #      Agent(id, neighs, p_cop, state)
+    #  Agent definition for the simulations with parameters:
+    #  `id          ::Int64 ` -> ID
+    #  `state       ::String ` -> Infection state (S, I, R)
+    #  `new_state   ::String ` -> Agent's state after meetings
+    #  `num_meets   ::Int64 ` -> Number of meetings in a time step
+    #  `recovery_t  ::Int64 ` -> Recovery time
+    #  `infection_t ::Int64 ` -> Time of infection
+    #  `contacts_t  ::Vector{Agent}` -> Contacts at time t
+    #  `age_group   ::Int64 ` -> Agent's age group (demographic)
+    #  `degree_t    ::Int64` -> Degree (Number of contacts) at time t
+    #  `p_cop       ::Float64 ` -> Cooperation probability
+    #  `at_home     ::Bool ` -> Flag to represent Agent is at home
+    #  """
+    #  mutable struct Agent
+    #      id          ::Int64
+    #      state       ::String
+    #      new_state   ::String
+    #      num_meets   ::Int64
+    #      recovery_t  ::Int64
+    #      infection_t ::Int64
+    #      contacts_t  ::Vector{Agent}
+    #      age_group   ::Int64
+    #      degree_t    ::Int64
+    #      p_cop       ::Float64
+    #      at_home     ::Bool
+    #      # DEFAULT CONSTRUCTOR
+    #      Agent(id) = new(id, "S", "S", 1, 5, 0, Vector{Agent}(), 1, 0, 1.0, false)
+    #  end
 
     ##=================####==============##
 
