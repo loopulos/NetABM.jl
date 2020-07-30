141c56c4bcb35d0776b4b155501bedd0d6660a59
Ollin <ollin.langle@ciencias.unam.mx>
2020-07-30 16:00:13 -0400

    added counter to stop looking at the whole array

--- examples/test.jl
+++ examples/test.jl
@@ -198,7 +198,7 @@ global Ihigh = Array{Float64}(undef,0)
 global Iall = Array{Array}(undef,0)
 global n_boot = 10
 global cil = 0.95
-global j
+global j = 0.1
         #  Threads.@threads
 global p = plot()
 begin
@@ -365,7 +365,118 @@ p=plot!(xlabel = L"t_i", ylabel = L"I/N")
 savefig(p,"../figs/sir_adapting_distance.png")
 
 
+#  Changing the cooperation according to neighboors infections given distance
 
+num_agents = 1000
+global Imean = Array{Float64}(undef,0)
+global Ilow = Array{Float64}(undef,0)
+global Ihigh = Array{Float64}(undef,0)
+global Iall = Array{Array}(undef,0)
+global n_boot = 10
+global cil = 0.95
+global j
+        #  Threads.@threads
+global p = plot()
+begin
+    for j in [0,1]
+        global Imean = Array{Float64}(undef,0)
+        global Ilow = Array{Float64}(undef,0)
+        global Ihigh = Array{Float64}(undef,0)
+        global Iall = Array{Array}(undef,0)
+        Threads.@threads for a in 1:100
+            g = barabasi_albert(num_agents,10)
+            agents = [Agent(i) for i = 1:num_agents]
+            init_demographics!(agents;states=["S","I"],initial=[0.9,0.1])
+            set_coop_agents!(agents;p_cop=0.4)
+            set_adapt_agents!(agents;p_cop=0.4)
+            map(x -> assign_contacts!(g,x), agents);
+            get_coop!(agents)
+            I = Array{Float64}(undef,0)
+            for i in 1:100
+                next_state!(agents;inf_prob=0.1, rec_prob=0.1, coop_red=0.8, R=false)
+                update_state!(agents)
+                update_coop_distance_inf!(agents,g,j,i;lrt=true)
+                get_coop!(agents)
+                push!(I,([ag.state for ag in agents if ag.state=="I"] |> length)/num_agents)
+            end
+            push!(Iall,I)
+            #  p = plot!(1:length(I),I,label=string(j))
+        end
+        #  end
+        for step in eachindex(Iall[1])
+            bs1 = bootstrap(mean,getindex.(Iall,step),BasicSampling(n_boot))
+            bs2 = bootstrap(std,getindex.(Iall,step),BasicSampling(n_boot))
+            bci1 = confint(bs1, BasicConfInt(cil))
+            bci2 = confint(bs2, BasicConfInt(cil))
+            push!(Imean,bci1[1][1])
+            push!(Ilow,bci2[1][2])
+            push!(Ihigh,bci2[1][3])
+        end
+        #  p=plot!(1:length(Imean), Imean, ribbon = (Imean .- Ilow, Ihigh .- Imean), fillalpha=0.35,label=string(j))
+        p=plot!(1:length(Imean), Imean, ribbon = (Ilow, Ihigh), fillalpha=0.35,label=string(j))
+    end
+end
+p
 
+p=plot!(xlims=(0,100),ylims=(0,1))
+p=plot!(xlabel = L"t_i", ylabel = L"I/N")
+savefig(p,"figs/sis_adapting_distance_infections_neighborhood.png")
+
+#  Changing the cooperation according to first neighboors infection
+
+num_agents = 1000
+global Imean = Array{Float64}(undef,0)
+global Ilow = Array{Float64}(undef,0)
+global Ihigh = Array{Float64}(undef,0)
+global Iall = Array{Array}(undef,0)
+global n_boot = 10
+global cil = 0.95
+global j
+        #  Threads.@threads
+global p = plot()
+begin
+    for j in [0.1, 0.4, 0.7, 1]
+        global Imean = Array{Float64}(undef,0)
+        global Ilow = Array{Float64}(undef,0)
+        global Ihigh = Array{Float64}(undef,0)
+        global Iall = Array{Array}(undef,0)
+        Threads.@threads for a in 1:100
+            g = barabasi_albert(num_agents,10)
+            agents = [Agent(i) for i = 1:num_agents]
+            init_demographics!(agents;states=["S","I"],initial=[0.9,0.1])
+            set_coop_agents!(agents;p_cop=0.4)
+            set_adapt_agents!(agents;p_cop=j)
+            map(x -> assign_contacts!(g,x), agents);
+            get_coop!(agents)
+            I = Array{Float64}(undef,0)
+            for i in 1:100
+                next_state!(agents;inf_prob=0.1, rec_prob=0.1, coop_red=0.8, R=false)
+                update_state!(agents)
+                update_coop_distance_inf!(agents,g,1,i;lrt=false)
+                get_coop!(agents)
+                push!(I,([ag.state for ag in agents if ag.state=="I"] |> length)/num_agents)
+            end
+            push!(Iall,I)
+            #  p = plot!(1:length(I),I,label=string(j))
+        end
+        #  end
+        for step in eachindex(Iall[1])
+            bs1 = bootstrap(mean,getindex.(Iall,step),BasicSampling(n_boot))
+            bs2 = bootstrap(std,getindex.(Iall,step),BasicSampling(n_boot))
+            bci1 = confint(bs1, BasicConfInt(cil))
+            bci2 = confint(bs2, BasicConfInt(cil))
+            push!(Imean,bci1[1][1])
+            push!(Ilow,bci2[1][2])
+            push!(Ihigh,bci2[1][3])
+        end
+        #  p=plot!(1:length(Imean), Imean, ribbon = (Imean .- Ilow, Ihigh .- Imean), fillalpha=0.35,label=string(j))
+        p=plot!(1:length(Imean), Imean, ribbon = (Ilow, Ihigh), fillalpha=0.35,label=string(j))
+    end
+end
+p
+
+p=plot!(xlims=(0,100),ylims=(0,1))
+p=plot!(xlabel = L"t_i", ylabel = L"I/N")
+savefig(p,"figs/sis_adapting_first_neighboors.png")
 
 
--- src/NetABM.jl
+++ src/NetABM.jl
@@ -35,45 +35,15 @@ module NetABM
         contacts_t  ::Vector{Int64}
         coopf       ::Vector{Int64}
         non_coopf   ::Vector{Int64}
+        counter     ::Int64
         age_group   ::Int64
         degree_t    ::Int64
         p_cop       ::Float64
         at_home     ::Bool
         adapter     ::Bool
         # DEFAULT CONSTRUCTOR
-        Agent(id) = new(id, "S", "S", Vector{String}(), 1, 5, 0, Vector{Int64}(), Vector{String}(), Vector{String}(), 1, 0, 1.0, false, true)
+        Agent(id) = new(id, "S", "S", Vector{String}(), 1, 5, 0, Vector{Int64}(), Vector{String}(), Vector{String}(), 0, 1, 0, 1.0, false, true)
     end
-
-    #  """
-    #      Agent(id, neighs, p_cop, state)
-    #  Agent definition for the simulations with parameters:
-    #  `id          ::Int64 ` -> ID
-    #  `state       ::String ` -> Infection state (S, I, R)
-    #  `new_state   ::String ` -> Agent's state after meetings
-    #  `num_meets   ::Int64 ` -> Number of meetings in a time step
-    #  `recovery_t  ::Int64 ` -> Recovery time
-    #  `infection_t ::Int64 ` -> Time of infection
-    #  `contacts_t  ::Vector{Agent}` -> Contacts at time t
-    #  `age_group   ::Int64 ` -> Agent's age group (demographic)
-    #  `degree_t    ::Int64` -> Degree (Number of contacts) at time t
-    #  `p_cop       ::Float64 ` -> Cooperation probability
-    #  `at_home     ::Bool ` -> Flag to represent Agent is at home
-    #  """
-    #  mutable struct Agent
-    #      id          ::Int64
-    #      state       ::String
-    #      new_state   ::String
-    #      num_meets   ::Int64
-    #      recovery_t  ::Int64
-    #      infection_t ::Int64
-    #      contacts_t  ::Vector{Agent}
-    #      age_group   ::Int64
-    #      degree_t    ::Int64
-    #      p_cop       ::Float64
-    #      at_home     ::Bool
-    #      # DEFAULT CONSTRUCTOR
-    #      Agent(id) = new(id, "S", "S", 1, 5, 0, Vector{Agent}(), 1, 0, 1.0, false)
-    #  end
 
     ##=================####==============##
 
